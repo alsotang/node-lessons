2b7b049a426552f07b0a3a68008e943ef2462097
alsotang <alsotang@gmail.com>
2014-10-06 01:39:47 +0800

    void 0

---
+++ lesson3/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+
+var app = express();
+
+app.get('/', function (req, res, next) {
+  superagent.get('https://cnodejs.org/')
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+      var $ = cheerio.load(sres.text);
+      var items = [];
+      $('#topic_list .topic_title').each(function (idx, element) {
+        var $element = $(element);
+        items.push({
+          title: $element.attr('title'),
+          href: $element.attr('href')
+        });
+      });
+
+      res.send(items);
+    });
+});
+
+
+app.listen(3000, function () {
+  console.log('app is listening at port 3000');
+});
